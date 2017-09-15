249af9d05edbd7fdc0e939308ba6aa16e1d94578
s21400426 <s21400426@CS-SR90NC844.stu.tuhsd.adn>
2017-09-15 11:22:46 -0700

    Added to lexar, removed parser, fixed bugs.

--- C--/src/compiler/Lexar.java
+++ C--/src/compiler/Lexar.java
@@ -59,6 +59,7 @@ public class Lexar {
 		}
 		catch(StringIndexOutOfBoundsException e){
 			addToken();
+			xpos++;
 			return;
 		}
 		catch(Exception e){
--- C--/src/test/CompilerTest.java
+++ C--/src/test/CompilerTest.java
@@ -1,10 +1,15 @@
 package test;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import compiler.Lexar;
 
 public class CompilerTest {
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		System.out.println(new BufferedReader(new FileReader(new File(args[0]))).readLine());
 		Lexar lexar = new Lexar(new File(args[0]));
 		lexar.listTokens();
 	}
