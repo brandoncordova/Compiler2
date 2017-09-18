fdb22c0f68bb019a1167d13e915ded6e0c8cdf8f
s21400426 <s21400426@CS-SR90NC844.stu.tuhsd.adn>
2017-09-18 12:00:44 -0700

    Fixed Lexar.

--- C--/src/compiler/Parser.java
+++ C--/src/compiler/Parser.java
@@ -1,1 +1,37 @@
+package compiler;
 
+import java.util.ArrayList;
+
+public class Parser {
+	private Lexar lexar;
+	private int i;
+	private ArrayList<Token> tokens;
+	public Parser(Lexar lexar){
+		this.lexar = lexar;
+		this.tokens = this.lexar.getTokens();
+		this.i = 0;
+		this.run();
+	}
+	private void parseAssign(){
+		
+	}
+	private void parseLine(){
+		if(this.tokens.get(i).type==Type.WORD){
+			switch(this.tokens.get(i).value){
+				case "var":
+					this.parseAssign();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	private void run(){
+		while(true){
+			this.parseLine();
+			if(!(this.tokens.get(this.i).value=="\n")){
+				break;
+			}
+		}
+	}
+}
